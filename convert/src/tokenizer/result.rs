b90ed17e388d94744327dc836f829ccd81a30082
bennyboer <barny.eder@gmail.com>
2023-04-03 00:25:55 +0200

    Removed tokenizer

--- convert/src/tokenizer/result.rs
+++
@@ -1,9 +0,0 @@
-use crate::util::SourcePosition;
-
-pub type TokenizeResult<T> = Result<T, TokenizeError>;
-
-#[derive(Debug)]
-pub struct TokenizeError {
-    pub message: String,
-    pub source_position: SourcePosition,
-}
